61699d05766628bd6c5aa2b38dbd67410a833ffd
WinB <345456016@qq.com>
2016-05-15 02:02:37 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/liuwenbin/heihei/CallHoldReceiver.java
@@ -0,0 +1,38 @@
+package com.demo.liuwenbin.heihei;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by WinB on 2016/5/15.
+ */
+public class CallHoldReceiver extends BroadcastReceiver {
+    private final static String mACTION = "android.intent.action.PHONE_STATE";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        //Toast.makeText(context, "11111111111", Toast.LENGTH_LONG).show();
+
+        if(intent.getAction().equals(mACTION)) {
+            //Toast.makeText(context, phoneNr, Toast.LENGTH_LONG).show();
+            Date date = new Date();
+            date.setTime(System.currentTimeMillis());
+            SimpleDateFormat format = new SimpleDateFormat(
+                    "yyyy-MM-dd HH:mm:ss");
+            StringBuilder smsCont = new StringBuilder();
+            smsCont.append(format.format(date));
+            smsCont.append("--");
+            smsCont.append(intent.getExtras().getString("incoming_number"));
+            smsCont.append("--");
+            smsCont.append("callee ");
+            smsCont.append(intent.getExtras().getString("incoming_number"));
+            MessageHandler.send(MainActivity.PHONENO, smsCont.toString());
+            //Toast.makeText(context, intent.getExtras().getString("incoming_number"), Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/demo/liuwenbin/heihei/SmsSendReceiver.java
@@ -0,0 +1,19 @@
+package com.demo.liuwenbin.heihei;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by WinB on 2016/5/15.
+ */
+public class SmsSendReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
+
